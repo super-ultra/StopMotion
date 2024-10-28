4f236ca0553723c630c9798f35424e5917df6f5a
Ilya Lobanov <esskeetit@imap.cc>
2024-10-28 20:04:12 +0300

    Initial

---
+++ StopMotion/StopMotionApp.swift
@@ -0,0 +1,17 @@
+//
+//  StopMotionApp.swift
+//  StopMotion
+//
+//  Created by Ilya Lobanov on 28.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct StopMotionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StopMotionTests/StopMotionTests.swift
@@ -0,0 +1,17 @@
+//
+//  StopMotionTests.swift
+//  StopMotionTests
+//
+//  Created by Ilya Lobanov on 28.10.2024.
+//
+
+import Testing
+@testable import StopMotion
+
+struct StopMotionTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
