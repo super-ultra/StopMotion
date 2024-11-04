1d34c6e3de0f8a269dcb534d6a270cd9db18f1a8
Ilya Lobanov <esskeetit@imap.cc>
2024-11-04 20:06:47 +0300

    Fix generation

---
+++ Packages/stop-motion-package/StopMotionToolbox/Sources/Functions.swift
@@ -0,0 +1,16 @@
+//
+//  Functions.swift
+//  stop-motion-package
+//
+//  Created by Ilya Lobanov on 03.11.2024.
+//
+
+import CoreGraphics
+
+extension FloatingPoint {
+    
+    public func degToRad() -> Self {
+        return .pi * self / 180
+    }
+    
+}
